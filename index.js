e6ae850f366a051bf42dfd57b8462c9c6bb4a7a0
Hoang Tuan Anh <o0pochieu0o@gmail.com>
2016-12-17 14:12:52 +0700

    Register Facebook Webhook

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: event.message.text});
+			 if (!social(event.sender.id, event.message.text)) {
+				sendMessage(event.sender.id, {text: event.message.text});
+			 }
         }
     }
     res.sendStatus(200);
