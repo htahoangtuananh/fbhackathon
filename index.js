d9002dc0129fc81fad1e681d0ee5a69ac0b4b2c2
Hoang Tuan Anh <o0pochieu0o@gmail.com>
2016-12-17 16:48:04 +0700

    Register Facebook Webhook

--- index.js
+++ index.js
@@ -25,18 +25,18 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
+        if (event.message.text) {
 			 if (!social(event.sender.id, event.message.text)) {
 				sendMessage(event.sender.id, {text: event.message.text});
 			 }
         }
-		if (event.message.attachments) {
+
 		//Checking if there are any image attachments 
 			if(event.message.attachments[0].type === "image"){
 			 var imageURL = event.message.attachments[0].payload.url;
 			 sendMessage(event.sender.id, {text:"This is transitory yellowing decease!"});
 			}
-	   }
+	   
 	}
     res.sendStatus(200);
 });
