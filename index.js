e2b18b13c2f3414a2be9a2a56386d6d47bf7a98f
Hoang Tuan Anh <o0pochieu0o@gmail.com>
2016-12-17 14:15:33 +0700

    Register Facebook Webhook

--- index.js
+++ index.js
@@ -80,40 +80,9 @@ function social(recipientId, text) {
 	{
 			var gender=getGender();
 			message="Welcome"+gender;
+			sendMessage(recipientId, message);
+			return true;
 	}
-    if (values.length === 3 && values[0] === 'kitten') {
-        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-            
-            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-            
-            message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": imageUrl ,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": imageUrl,
-                                "title": "Show kitten"
-                                }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
-                    }
-                }
-            };
-    
-            sendMessage(recipientId, message);
-            
-            return true;
-        }
-    }
     
     return false;
     
